95fd5dcc3627100c52214d9d0f7109ed625360e3
Gulshan Kumar <inbox.gulshan@outlook.com>
2021-11-27 18:52:57 +0530

    Added Initial Setup for Binary Tree

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Node
+    {
+        internal int value;
+        internal Node? left;
+        internal Node? right;
+
+        public Node(int nodeValue)
+        {
+            value = nodeValue;
+            left = null;
+            right = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public Node CreateBinaryTree()
+        {
+            Node rootNode = new Node(0);
+
+            Node leftSubTreeRoot = new Node(10);
+            leftSubTreeRoot.left = new Node(20);
+            leftSubTreeRoot.right = new Node(22);
+
+            Node rightSubTreeRoot = new Node(11);
+            rightSubTreeRoot.left = new Node(21);
+            rightSubTreeRoot.right = new Node(23);
+
+            rootNode.left = leftSubTreeRoot;
+            rootNode.right = rightSubTreeRoot;
+
+            return rootNode;
+        }
+    }
+}

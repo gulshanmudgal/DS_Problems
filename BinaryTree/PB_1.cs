d63562e030ed076b8651d5a9da1dd93a212039d8
Gulshan Kumar <inbox.gulshan@outlook.com>
2021-11-27 19:31:41 +0530

    Solved Leetcode 101

---
+++ BinaryTree/PB_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public static class PB_1
+    {
+        /// <summary>
+        /// 101. Symmetric Tree: https://leetcode.com/problems/symmetric-tree/
+        /// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static bool IsSymmetric(Node root)
+        {
+            return root == null || IsChildSymmetric(root.left, root.right);
+        }
+
+        private static bool IsChildSymmetric(Node? left, Node? right)
+        {
+            if (left == null || right == null)
+            {
+                return left == right;
+            }
+
+            if (left.value != right.value)
+            {
+                return false;
+            }
+
+            return IsChildSymmetric(left.left, right.right) && IsChildSymmetric(left.right, right.left);
+        }
+    }
+}

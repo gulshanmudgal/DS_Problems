b08d33f01650b6f09dc7f2f65d30d58968a105ce
Gulshan Kumar <inbox.gulshan@outlook.com>
2021-11-28 08:17:19 +0530

    Find path to the node from. Solved Leetcode 236.

---
+++ BinaryTree/PB_2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class PB_2
+    {
+        private static bool GetPath(Node? root, Stack<int> path, int searchVal)
+        {
+            if (root == null)
+                return false;
+
+            path.Push(root.value);
+
+            if(root.value == searchVal)
+            {
+                return true;
+            }
+
+            if(GetPath(root.left, path, searchVal) || GetPath(root.right, path, searchVal))
+            {
+                return true;
+            }
+
+            path.Pop();
+            return false;
+        }
+
+        public List<int> findNodePath(Node? root, int searchVal)
+        {
+            Stack<int> path = new Stack<int>();
+
+            if(root == null)
+            {
+                return path.ToList();
+            }
+
+            GetPath(root, path, searchVal);
+
+            var finalPath = path.ToList();
+            finalPath.Reverse();
+            return path.ToList();
+        }
+    }
+}
---
+++ BinaryTree/PB_3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class PB_3
+    {
+        /// <summary>
+        /// 236. Lowest Common Ancestor of a Binary Tree https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+        /// Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public Node LowestCommonAncestor(Node? root, Node p, Node q)
+        {
+            if (root == null || p == root || q == root)
+            {
+                return root;
+            }
+
+            Node left = LowestCommonAncestor(root.left, p, q);
+            Node right = LowestCommonAncestor(root.right, p, q);
+
+            if (left == null)
+            {
+                return right;
+            }
+            else if (right == null)
+            {
+                return left;
+            }
+            else
+            {
+                return root;
+            }
+        }
+    }
+}

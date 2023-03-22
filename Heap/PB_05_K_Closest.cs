7e1225a3da0247ca8785749c1efae0dce1ba0ca5
Gulshan Kumar <inbox.gulshan@outlook.com>
2023-03-22 23:01:06 +0530

    Leetcode 658: Solved K closest numbers.

---
+++ Heap/PB_05_K_Closest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    internal class PB_05_K_Closest
+    {
+        public static IList<int> FindClosestElements(int[] arr, int k, int x)
+        {
+            IList<int> result = new List<int>();
+            PriorityQueue<int, int> maxHeap = new PriorityQueue<int, int>();
+
+            foreach (var number in arr)
+            {
+                maxHeap.Enqueue(number, (Int32.MaxValue - Math.Abs(x - number)));
+
+                if(maxHeap.Count > k)
+                {
+                    maxHeap.Dequeue();
+                }
+            }
+
+            int count = maxHeap.Count;
+
+            for(int i = 0; i < count; i++)
+            {
+                result.Add(maxHeap.Dequeue());
+            }
+
+            return result;
+        }
+    }
+}
--- Heap/Program.cs
+++ Heap/Program.cs
@@ -13,3 +13,7 @@ Console.WriteLine(String.Join(',', resultPB03));
 int[] nearlySortedInput = new int[] { 6, 5, 3, 2, 8, 10, 9 };
 int[] resultPB04 = PB_04_Sort_K_Sorted.GetNearlySorted(nearlySortedInput, 3);
 Console.WriteLine(String.Join(',', resultPB04));
+
+int[] inputPB05 = new int[5] { 1, 2, 3, 4, 5 };
+IList<int> resultPB05 = PB_05_K_Closest.FindClosestElements(inputPB05, 4, 3);
+Console.WriteLine(String.Join(',', resultPB05));
